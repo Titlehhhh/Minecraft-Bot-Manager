eeabe97b6bbe3fcdc84111e125739004f06b78a2
Titlehhhh <93156853+Titlehhhh@users.noreply.github.com>
2022-02-05 08:12:37 +0300

    Удаление некоторых файлов

--- PacketPallete340/Packets/Server/Game/World/ServerChunkDataPacket.cs
+++ PacketPallete340/Packets/Server/Game/World/ServerChunkDataPacket.cs
@@ -1,6 +1,7 @@
 using MinecraftLibrary.API.Protocol;
 using MinecraftLibrary.API.Protocol.Attributes;
 using MinecraftLibrary.API.Protocol.Helpres;
+using MinecraftLibrary.API.World;
 
 namespace PacketPallete340.Packets.Server.Game.World
 {
@@ -8,19 +9,24 @@ namespace PacketPallete340.Packets.Server.Game.World
     [PacketMeta(0x20, 340, PacketSide.Server, PacketCategory.Game)]
     public class ServerChunkDataPacket : MinecraftPacket
     {
+        public IChunkColumn Column { get; set; }
         //int x = in.readInt();
-       //int z = in.readInt();
-       //boolean fullChunk = in.readBoolean();
-       //int chunkMask = in.readVarInt();
-       //byte data[] = in.readBytes(in.readVarInt());
-       //CompoundTag[] tileEntities = new CompoundTag[in.readVarInt()];
-       //for(int i = 0; i < tileEntities.length; i++) {
-       //tileEntities[i] = NetUtil.readNBT(in);
-       //}
-       //
-       //this.column = NetUtil.readColumn(data, x, z, fullChunk, false, chunkMask, tileEntities);
+        //int z = in.readInt();
+        //boolean fullChunk = in.readBoolean();
+        //int chunkMask = in.readVarInt();
+        //byte data[] = in.readBytes(in.readVarInt());
+        //CompoundTag[] tileEntities = new CompoundTag[in.readVarInt()];
+        //for(int i = 0; i < tileEntities.length; i++) {
+        //tileEntities[i] = NetUtil.readNBT(in);
+        //}
+        //
+        //this.column = NetUtil.readColumn(data, x, z, fullChunk, false, chunkMask, tileEntities);
         public override void Read(MinecraftStream output)
         {
+            int x = output.ReadNextInt();
+            int z = output.ReadNextInt();
+            bool fullChunk = output.ReadNextBool();
+            byte[] data = output.ReadNextByteArray();
             
         }
         public ServerChunkDataPacket() {}
