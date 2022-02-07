a1c4cdfb1e4bbd17aa2deee18bed8bfbedbd9816
Titlehhhh <93156853+Titlehhhh@users.noreply.github.com>
2022-02-07 17:37:20 +0300

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string[] list = {"BANANA","CARS","HOUSE","WOMEN","UNITED","HAPPY","CUPBOARD" };
+var query = list.GroupBy(x => x.Length).OrderBy(x => x.Key)
+.SelectMany(x => x.OrderByDescending(c => c)).ToArray();
+Console.WriteLine(string.Join("\n",query));
 Console.ReadLine();
