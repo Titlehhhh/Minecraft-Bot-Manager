07bc2794e10bb1e4d5b7959efabf9dbda01627f1
Titlehhhh <93156853+Titlehhhh@users.noreply.github.com>
2022-02-08 17:22:39 +0300

    Добавлен IBotObjecy

--- MinecraftLibrary.API/Bot/IBotObject.cs
+++ MinecraftLibrary.API/Bot/IBotObject.cs
@@ -4,11 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using MinecraftLibrary.API.Networking.Proxy;
+using MinecraftLibrary.API.Networking;
+using MinecraftLibrary.API.Protocol;
 
 namespace MinecraftLibrary.API.Bot
 {
     public interface IBotObject : INotifyPropertyChanged
     {
+        public void Connect();
+        public void Reconnect();
+        public void Disconect();
 
+        public string Host { get; set; }
+        public ushort Port { get; set; }
+        public ProxyInfo? Proxy { get; set; }
+
+        public ITcpClientSession Session { get; set; }
+        public IPacketReader Reader { get; set; }
+        public IPacketWriter Writer { get; set; }
+
+
+        public SubProtocol SubProtocol { get; }
+    }
+    public enum SubProtocol
+    {
+        Handshake,
+        Login,
+        Game
     }
 }
