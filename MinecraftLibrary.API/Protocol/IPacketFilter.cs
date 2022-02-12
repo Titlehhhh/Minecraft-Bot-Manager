e99a83e07faefa492ec086653239fdb75bc8cd62
Titlehhhh <93156853+Titlehhhh@users.noreply.github.com>
2022-02-12 14:11:16 +0300

    1

---
+++ MinecraftLibrary.API/Protocol/IPacketFilter.cs
@@ -0,0 +1,32 @@
+﻿using MinecraftLibrary.API.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinecraftLibrary.API.Protocol
+{
+    public interface IPacketFilter
+    {
+        Dictionary<int, Type> ServerPackets { get; set; }
+        Dictionary<Type, int> ClientPackets { get; set; }
+
+        void RegisterClientPacket(int id, Type value);
+        void RegisterrClientPacket<TPacket>(int id) where TPacket : IPacket;
+
+        void UnregisterClientPacket<TPacket>();
+        void UnregisterClientPacket(int id);
+
+        void RegisterServerPacket(int id, Type value);
+        void RegisterServerPacket<TPacket>(int id) where TPacket : IPacket;
+
+        void UnregisterServerPacket(Type key);
+        void UnregisterServerPacket<TPacket>() where TPacket : IPacket;
+        void UnregisterServerPacket(int id);
+
+
+    }
+
+}
---
+++ MinecraftLibrary.API/Protocol/ITcpClientSession.cs
@@ -0,0 +1,21 @@
+﻿using MinecraftLibrary.API.Networking.IO;
+using MinecraftLibrary.API.Networking.Proxy;
+
+namespace MinecraftLibrary.API.Protocol
+{
+    public interface ITcpClientSession : IDisposable
+    {
+        void Connect();
+        void Close();
+
+        void SwitchEncryption(byte[] key);
+
+        MinecraftStream NetStream { get; }
+        int CompressionThreshold { get; set; }
+
+        string Host { get; set; }
+        int Port { get; set; }
+        ProxyInfo? Proxy { get; set; }
+    }
+
+}
