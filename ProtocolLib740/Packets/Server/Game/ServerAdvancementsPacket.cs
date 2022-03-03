6d34af15993cf2f20415907a239e1ed27d4783e0
Titlehhhh <93156853+Titlehhhh@users.noreply.github.com>
2022-03-03 18:50:47 +0500

    1

--- ProtocolLib740/Packets/Server/Game/ServerAdvancementsPacket.cs
+++ ProtocolLib740/Packets/Server/Game/ServerAdvancementsPacket.cs
@@ -1,23 +1,24 @@
 using MinecraftLibrary.API;
 using MinecraftLibrary.API.IO;
 using MinecraftLibrary.API.Networking;
-using MinecraftLibrary.API.Protocol;
+using MinecraftLibrary.API.Protocol;
 
 
 namespace ProtocolLib740.Packets.Server
-{
+{
 
-    [PacketInfo(0x57, 740, PacketCategory.Game, PacketSide.Server)]    public class ServerAdvancementsPacket : IPacket
+    [PacketInfo(0x57, 740, PacketCategory.Game, PacketSide.Server)]
+    public class ServerAdvancementsPacket : IPacket
     {
         public void Write(IMinecraftStreamWriter stream)
-        {
+        {
 
         }
         public void Read(IMinecraftStreamReader stream)
-        {
+        {
 
         }
         public ServerAdvancementsPacket() { }
     }
-}
+}
 
